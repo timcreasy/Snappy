d9c18e4f6bd83b4c4d9311dc5c18693b47bed6ce
Tim Creasy <timcreasy@me.com>
2016-07-29 17:21:17 -0500

    Markers implemented, no picture

--- www/app/controllers/SnapMapCtrl.js
+++ www/app/controllers/SnapMapCtrl.js
@@ -1,43 +1,45 @@
 "use strict";
 
-snappy.controller('SnapMapCtrl', function($scope, $state, $cordovaGeolocation) {
+snappy.controller('SnapMapCtrl', function($scope, $state, $cordovaGeolocation, MapFactory) {
+
+  MapFactory.init();
 
   $scope.goHome = function() {
     $state.go('home');
   }
 
-  var options = {timeout: 10000, enableHighAccuracy: true};
-  $cordovaGeolocation.getCurrentPosition(options).then(function(position){
-
-    var latLng = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
-
-    var mapOptions = {
-      center: latLng,
-      zoom: 15,
-      mapTypeId: google.maps.MapTypeId.ROADMAP
-    };
-
-    $scope.map = new google.maps.Map(document.getElementById("map"), mapOptions);
-    //Wait until the map is loaded
-    google.maps.event.addListenerOnce($scope.map, 'idle', function(){
-
-      var marker = new google.maps.Marker({
-         map: $scope.map,
-         animation: google.maps.Animation.DROP,
-         position: latLng
-      });
-
-      var infoWindow = new google.maps.InfoWindow({
-         content: "Here I am!"
-      });
-
-      google.maps.event.addListener(marker, 'click', function () {
-         infoWindow.open($scope.map, marker);
-      });
-
-    });
-
-  }, function(error){
-    console.log("Could not get location");
-  });
+  // var options = {timeout: 10000, enableHighAccuracy: true};
+  // $cordovaGeolocation.getCurrentPosition(options).then(function(position){
+  //
+  //   var latLng = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
+  //
+  //   var mapOptions = {
+  //     center: latLng,
+  //     zoom: 15,
+  //     mapTypeId: google.maps.MapTypeId.ROADMAP
+  //   };
+  //
+  //   $scope.map = new google.maps.Map(document.getElementById("map"), mapOptions);
+  //   //Wait until the map is loaded
+  //   google.maps.event.addListenerOnce($scope.map, 'idle', function(){
+  //
+  //     var marker = new google.maps.Marker({
+  //        map: $scope.map,
+  //        animation: google.maps.Animation.DROP,
+  //        position: latLng
+  //     });
+  //
+  //     var infoWindow = new google.maps.InfoWindow({
+  //        content: "Here I am!"
+  //     });
+  //
+  //     google.maps.event.addListener(marker, 'click', function () {
+  //        infoWindow.open($scope.map, marker);
+  //     });
+  //
+  //   });
+  //
+  // }, function(error){
+  //   console.log("Could not get location");
+  // });
 });
---
+++ www/app/services/PictureMarkers.js
@@ -0,0 +1,26 @@
+snappy.factory('PictureMarkers', function($http, CurrentUser, FirebaseCreds) {
+
+  var markers = [];
+
+  return {
+    getMarkers: function(){
+
+      var queryString = `${FirebaseCreds.databaseURL}/picturemessages.json?orderBy="senderId"&equalTo="${CurrentUser.getUser().uid}"`;
+
+      return $http.get(queryString).then(function(response){
+          markers = response;
+          return markers;
+      });
+
+      // firebase.database().ref('picturemessages').orderByChild('senderId').equalTo(CurrentUser.getUser().uid).on('value', function(snapshot) {
+      //     var userPictures = snapshot.val();
+      //     markers = [];
+      //     for (var picture in userPictures) {
+      //       markers.push(userPictures[picture]);
+      //     }
+      //     return markers;
+      // });
+
+    }
+  }
+});
