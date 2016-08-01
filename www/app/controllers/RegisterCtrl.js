8808f3ee1d2e33050b57470e5841bc9e3ca04afa
Tim Creasy <timcreasy@me.com>
2016-08-01 15:39:06 -0500

    Added ability to add user profile images

--- www/app/controllers/RegisterCtrl.js
+++ www/app/controllers/RegisterCtrl.js
@@ -1,7 +1,71 @@
 "use strict";
 
 snappy.controller('RegisterCtrl',
-  function($scope, $ionicPlatform, Auth, $state) {
+  function($scope, $ionicPlatform, Auth, $state, $cordovaImagePicker, $cordovaCamera) {
+
+    var options = {
+     maximumImagesCount: 1,
+     width: 384,
+     height: 490,
+     quality: 50
+    };
+
+    $scope.profilePicture = null;
+
+    $scope.getProfilePicture = function() {
+      $cordovaImagePicker.getPictures(options)
+      .then(function (results) {
+        toDataUrl(results[0], function(base64Img) {
+          $scope.$apply(function() {
+            $scope.profilePicture = base64Img;
+          });
+        });
+      }, function(error) {
+        // error getting photos
+      });
+    };
+
+    $scope.takeProfilePicture = function() {
+
+      var options = {
+        quality: 50,
+        destinationType: Camera.DestinationType.DATA_URL,
+        sourceType: Camera.PictureSourceType.CAMERA,
+        allowEdit: false,
+        encodingType: Camera.EncodingType.PNG,
+        targetWidth: 384,
+        targetHeight: 490,
+        popoverOptions: CameraPopoverOptions,
+        saveToPhotoAlbum: false,
+  	    correctOrientation:true,
+        cameraDirection: 1
+      };
+
+      $cordovaCamera.getPicture(options).then(function(imageData) {
+        $scope.profilePicture = "data:image/png;base64," + imageData;
+      });
+
+    };
+
+    function toDataUrl(src, callback, outputFormat) {
+      var img = new Image();
+      img.crossOrigin = 'Anonymous';
+      img.onload = function() {
+        var canvas = document.createElement('CANVAS');
+        var ctx = canvas.getContext('2d');
+        var dataURL;
+        canvas.height = this.height;
+        canvas.width = this.width;
+        ctx.drawImage(this, 0, 0);
+        dataURL = canvas.toDataURL(outputFormat);
+        callback(dataURL);
+      };
+      img.src = src;
+      if (img.complete || img.complete === undefined) {
+        img.src = "data:image/gif;base64,R0lGODlhAQABAIAAAAAAAP///ywAAAAAAQABAAACAUwAOw==";
+        img.src = src;
+      }
+    }
 
     // Login button pressed in navbar
     $scope.loginPressed = function() {
@@ -17,7 +81,7 @@ snappy.controller('RegisterCtrl',
 
     // Register button pressed
     $scope.registerPressed = function() {
-      Auth.register($scope.register.email, $scope.register.password, $scope.register.fullName);
+      Auth.register($scope.register.email, $scope.register.password, $scope.register.fullName, $scope.profilePicture);
     };
 
 });
--- www/app/services/FirebaseInteraction.js
+++ www/app/services/FirebaseInteraction.js
@@ -3,11 +3,12 @@
 snappy.service("FirebaseInteraction", function($state) {
 
   // Adds new user to firebase
-  this.addNewUserToFirebase = function(uid, email, fullName) {
+  this.addNewUserToFirebase = function(uid, email, fullName, userPicture) {
     firebase.database().ref().child('users').child(uid).set({
       uid: uid,
       email: email,
-      fullName: fullName
+      fullName: fullName,
+      profilePicture: userPicture
     });
   };
 
